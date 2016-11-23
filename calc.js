686084ed2fab1b13b7ee28aea7195c6299243f4e
Luc Pascal Schroots <lucpschroots@hotmail.com>
2016-11-23 11:39:37 +0100

    Updated! Finished

--- calc.js
+++ calc.js
@@ -1,3 +1,33 @@
-	function displynum(n1){
-		calcform.txt1.value=calcform.txt1.value+n1;
+function c(val){
+	document.getElementById("display").value=val;
+}
+
+function math(val){
+	console.log(val);
+	if (!lastchar) {
+		if (val == "00" || val == "0" || val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9" || val == ".") {
+			document.getElementById("display").value+=val;
+		} else {
+			console.log(val);
+			document.getElementById("display").value+=val;
+			lastchar = true;
+		}
+
 	}
+	else{ 
+	if (val == "0" || val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9" || val == ".") {
+			document.getElementById("display").value+=val; lastchar = false }
+		}
+}
+
+function e(){
+	try{
+		c(eval(document.getElementById("display").value));
+	}
+	
+	catch(e){
+		c('Error')
+	}
+}
+
+var lastchar = false;
