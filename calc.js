77a6c052bad881d99d51d904dc15d0c3fcf8bf4b
Luc Pascal Schroots <lucpschroots@hotmail.com>
2016-12-13 09:54:33 +0100

    Updated error

--- calc.js
+++ calc.js
@@ -15,7 +15,7 @@ function math(val){
 
 	}
 	else{ 
-	if (val == "0" || val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9" || val == ".") {
+	if (val == "00" || val == "0" || val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9" || val == ".") {
 			document.getElementById("display").value+=val; lastchar = false }
 		}
 }
